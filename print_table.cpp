57269b9de5783c55954ae69ab14936e4ee57b3ea
Dart Basic to Advanced <mhlokman10@gmail.com>
2025-01-25 23:34:12 +0600

    print table 2, 3 , 4, 5

---
+++ print_table.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int i;
+//     for(i=2;i<=20;i+=2){
+//         cout<<i<<endl;
+//     }
+// }
+
+#include <iostream>
+using namespace std;
+int main(){
+    int i;
+    for(i=4;i<=40;i+=4){
+        cout<<i<<endl;
+    }
+}
