68781bd504106776e26645a722355d0c155a8a71
Md. Lokman Hossen <mhlokman10@gmail.com>
2026-01-06 23:23:29 +0600

    Time: 0 ms (100%), Space: 8.3 MB (11.53%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int hammingWeight(int n) {
 
-        int cnt = 0;
-        for(int i = 0; i<32; i++)
-        {
-            if((n & (1 << i)) != 0) cnt++;
-        }
-        return cnt;
+        // int cnt = 0;
+        // for(int i = 0; i<32; i++)
+        // {
+        //     if((n & (1 << i)) != 0) cnt++;
+        // }
+        // return cnt;
+
+        return __builtin_popcount(n);
         
     }
 };
