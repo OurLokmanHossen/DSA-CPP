904f9d53f326b5c8e536aada8c560f819be7c7b6
Md. Lokman Hossen <mhlokman10@gmail.com>
2026-01-08 14:17:25 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
 
-    //    if(n < 0) return false;
-    //    if(__builtin_popcount(n) == 1) return true;
-    //    else return false;
+       if(n < 0) return false;
+       if(__builtin_popcount(n) == 1) return true;
+       else return false;
 
-    if(n <= 0) return false;
-
-     if((n & (n-1)) == 0) return true;
-     else return false;
-    }
+   
 };
