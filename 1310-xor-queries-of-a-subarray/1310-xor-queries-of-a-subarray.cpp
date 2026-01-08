9495d6a5bd9d9a7f025caecdb9c30f77135c9d20
Md. Lokman Hossen <mhlokman10@gmail.com>
2026-01-09 00:47:02 +0600

    Time: 0 ms (100%), Space: 92.6 MB (34.14%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        
+        int n = arr.size();
+
+        vector<int> cumXor(n,0);
+
+        cumXor[0] = arr[0];
+
+        for(int i = 1; i<n; i++)
+        {
+          cumXor[i] = cumXor[i-1] ^ arr[i];
+        }
+
+        vector<int> result;
+
+        for(vector<int> &query: queries)
+        {
+            int L = query[0];
+            int R = query[1];
+
+            int Xor_val = cumXor[R] ^ (L == 0 ? 0 : cumXor[L-1]);
+
+            result.push_back(Xor_val);
+        }
+
+        return result;
+       
+    }
+};
