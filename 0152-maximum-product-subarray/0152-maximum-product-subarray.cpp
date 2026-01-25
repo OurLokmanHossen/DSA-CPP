32c0d53c12dda11b5e2fe28040251efd4f65a5b7
Md. Lokman Hossen <mhlokman10@gmail.com>
2026-01-25 12:46:32 +0600

    Time: 0 ms (100%), Space: 17.7 MB (85.09%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,15 +4,19 @@ public:
 
         int n = nums.size();
 
-       int bestend = nums[0];
-        int ans = nums[0];
+       int maxend = nums[0];
+       int minend = nums[0];
+
+       int ans = nums[0];
 
         for(int i = 1; i<n; i++)
         {
-            int v1 = bestend * nums[i];
-            int v2 = nums[i];
-            bestend = max(v1,v2);
-            ans = max(ans, bestend);
+            int v1 = maxend * nums[i];
+            int v2 = minend * nums[i];
+            int v3 = nums[i];
+            maxend = max({v1,v2,v3});
+            minend = min({v1,v2, v3});
+            ans = max(ans, max(maxend,minend));
         }
 
         return ans ;
